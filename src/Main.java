b370d351ab671104089b3186d58fd15fcc92111b
KamranGoudarzii <95715120+KamranGoudarzii@users.noreply.github.com>
2022-06-17 11:35:11 +0500

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args)
+    {
+        OurLibary obj = new OurLibary();
+        obj.add_item("A",65);
+        obj.add_item("B",66);
+        obj.add_item("C",67);
+
+        obj.display();
+        System.out.println("Hello");
+
+    }
+
+}
